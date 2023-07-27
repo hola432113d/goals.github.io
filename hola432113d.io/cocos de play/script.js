df61ec912080a27409768ba2cbd19599849c1f62
hola432113d <140765152+hola432113d@users.noreply.github.com>
2023-07-27 19:50:29 -0400

    ff

---
+++ hola432113d.io/cocos de play/script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const redirectionButton = document.getElementById('redirection-button');
+  
+    redirectionButton.addEventListener('click', function () {
+      // Cambiar la URL a la página de destino
+      window.location.href = 'file:///C:/Users/benja/Downloads/¿hola/secreto.html';
+    });
+  });
+  
